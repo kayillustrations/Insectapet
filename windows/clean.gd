61fb237d921225dd71bf9477566468f7064bc647
Kaylee (Kay) <kayddacraft@gmail.com>
2025-11-14 15:56:14 -0500

    cont

--- windows/clean.gd
+++ windows/clean.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
+@onready var fog: Sprite2D = $Fog
+
+const SIZE = Vector2(2000,1050)
+
+var imageTexture : ImageTexture
+var image : Image
+var radius = 10.0
+var erase = Color(0.0, 0.0, 0.0, 0.0)
+
 var click_pos: Array
 
 func _input(event: InputEvent) -> void:
 	if visible && not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		return
-	click_pos.append(get_local_mouse_position())
+	var mouse_pos = get_local_mouse_position()
+	if abs(mouse_pos.x) > SIZE.x/2:return
+	if abs(mouse_pos.y) > SIZE.y/2 + get_parent().position.y:return #TODO RETURN HERE
+	click_pos.append(mouse_pos)
 	queue_redraw()
 
 func _draw() -> void:
