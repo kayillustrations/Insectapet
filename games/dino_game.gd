c081d9101f889e470bb86103c42dd73b7354253e
Kaylee (Kay) <77746492+kayillustrations@users.noreply.github.com>
2025-11-23 15:31:12 -0500

    more dino code

--- games/dino_game.gd
+++ games/dino_game.gd
@@ -1,45 +1,68 @@
 extends Node2D
 
+#PRELOADS
+var ground_obs1 = preload()
+var ground_obs2 = preload()
+var ground_obs3 = preload()
+var air_obs = preload()
+var obstacles_ground := [ground_obs1,ground_obs2,ground_obs3]
+var obstacles_generated: Array
+var last_obs
+var air_heights: Array
+
+#VARS
 const BUG_START_POS = Vector2i(90,300)
 const CAM_START_POS = Vector2i.ZERO
 const SPEED_START = 10
 const SPEED_MAX = 25
+const SPEED_MOD = 5000
 
-@onready var bug = $Bug
-@onready var HUD = $"../HUD"
-@onready var score_label = HUD.find_child("Score")
-@onready var highscore_label = HUD.find_child("Highscore")
-@onready var start_label = HUD.find_child("Start")
+var bug
+var HUD
+var score_label: Label
+var highscore_label: Label
+var start_label: Label
 
 var score:int = 0
 var highschore:int = 0
 var speed:float
 
+var has_been_config = false
 var game_started:bool = false
 
-func _ready():
-	
+func Activated():
+	if !has_been_config:
+		bug = $Bug
+		HUD = $"../HUD"
+		score_label = HUD.find_child("Score")
+		highscore_label = HUD.find_child("Highscore")
+		start_label = HUD.find_child("Start")
+		has_been_config = true
 	new_game()
-	pass
 
 func new_game():
 	#reset variables
 	score = 0
-	#highscore = GameManage
+	#highscore = GameManager
+	
 	#reset nodes
 	bug.position = BUG_START_POS
 	bug.velocity = Vector2i.ZERO
+	speed = SPEED_START
 	$Camera2D.position = CAM_START_POS
 	
 	game_started = false
 	start_label.visible = true
+	Generate_Obj()
 
 func _process(delta):
 	if !game_started:
 		if Input.is_anything_pressed():
 			start_label.visible = false
 		return
-	speed = SPEED_START
+	speed = SPEED_START + (score/SPEED_MOD)
+	if speed > SPEED_MAX:
+		speed = SPEED_MAX
 
 	bug.position.x += speed
 	$Camera2D.position.x += speed
@@ -51,3 +74,16 @@ func UpdateScore():
 	score_label.text = "Distance: "
 	if score > highschore:
 		highscore_label.text = str(score)
+
+func Generate_Obj():
+	#ground obs
+	if obstacles_generated.is_empty():
+		var chosen = obstacles_ground[randi() % obstacles_ground.size()]
+		var obs = chosen.instantiate()
+		last_obs = obs
+		obstacles_generated.append(obs)
+
+	#air obs
+
+
+	pass
